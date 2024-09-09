aa01fc3cc4763cabdf4e085498db1a5f6e17a591
STRunit <st21unit@gmail.com>
2024-09-09 16:43:01 +0800

    Fix login page logic

--- FE/src/components/login-form.jsx
+++ FE/src/components/login-form.jsx
@@ -1,41 +1,47 @@
 "use client"
 
-import { useRef, useState } from "react";
+import { useMemo, useState } from "react";
 import { CustomButton } from "./curstomButton"
 import { CustomInput } from "./customInput"
 import { axiosInstance } from "@/lib/axios";
+import { useRouter } from "next/navigation";
+import _ from "lodash"
 
 export const LoginForm = () => {
+    const router = useRouter();
+    const [formData, setFormData] = useState({ email: "", password: ""});
 
-    const formRef = useRef(null);
-    const [name, setName] = useState("")
-
-    const handleOnchange = (e) => {
-        setName(e.target.value)
+    const handleOnChange = (event) => {
+        setFormData((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value,
+          }));
     }
 
+    const debounceFn = useMemo(() => _.debounce(handleOnChange, 500), []);
+  
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const formData = new FormData(formRef.current);
-        const { email, password } = Object.fromEntries(formData.entries());
 
         try {
             const response = await axiosInstance.post("/auth/login", {
                 email: formData.email,
-                password: formData.password
+                password: formData.password,
             })
-            console.log(response);
+
+            if (response.status === 200) router.push("/")
+            
         } catch (error) {
             console.error(error);
         }
     };
 
-    return <form ref={formRef} onSubmit={handleSubmit} className="inline-flex p-8 flex-col items-start rounded-2xl bg-white gap-12">
+    return <form onSubmit={handleSubmit} className="inline-flex p-8 flex-col items-start rounded-2xl bg-white gap-12">
         <h2 className="text-[#0D1118] text-center text-[28px] font-bold">Нэвтрэх</h2>
         <div className="flex w-[384px] flex-col items-start gap-2">
             <div className="flex flex-col items-start gap-4 self-stretch">
-                <CustomInput name="email" type="email" onchange={handleOnchange} label={"Имэйл"} placeholder={"Имэйл хаягаа оруулна уу"} />
-                <CustomInput name="password" type="password" onchange={handleOnchange} label={"Нууц үг"} placeholder={"Нууц үг"} showIcon={true} />
+                <CustomInput name="email" type="email" onchange={debounceFn} label={"Имэйл"} placeholder={"Имэйл хаягаа оруулна уу"} />
+                <CustomInput name="password" type="password" onchange={debounceFn} label={"Нууц үг"} placeholder={"Нууц үг"} showIcon={true} />
             </div>
             <p className="flex justify-end items-center text-sm self-stretch text-[#3F4145]">
                 Нууц үг сэргээх
@@ -43,9 +49,9 @@ export const LoginForm = () => {
         </div>
 
         <div className="flex flex-col items-center gap-8">
-            <CustomButton type="submit" variant={name !== "" ? "primary" : "secondary"}>Нэвтрэх</CustomButton>
+            <CustomButton type="submit" variant={formData.email && formData.password !== "" ? "primary" : "secondary"}>Нэвтрэх</CustomButton>
             <p className="text-sm text-[#3F4145]">Эсвэл</p>
-            <CustomButton variant="outline">Бүртгүүлэх</CustomButton>
+            <CustomButton type="button" variant="outline">Бүртгүүлэх</CustomButton>
         </div>
     </form>
 }
