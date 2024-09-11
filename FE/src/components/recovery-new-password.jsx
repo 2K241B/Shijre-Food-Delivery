645159f5ee39cddcc372d1def07e422fb74b9150
STRunit <st21unit@gmail.com>
2024-09-11 15:11:10 +0800

    Add forgot password only ui

--- FE/src/components/recovery-new-password.jsx
+++ FE/src/components/recovery-new-password.jsx
@@ -4,6 +4,7 @@ import { useMemo, useState } from "react";
 import { CustomButton } from "./curstomButton"
 import { CustomInput } from "./customInput"
 import _ from "lodash"
+import { CheckIcon } from "lucide-react";
 
 export const RecoveryNewPassword = () => {
 
@@ -16,8 +17,13 @@ export const RecoveryNewPassword = () => {
     }
 
     const debounceFn = useMemo(() => _.debounce(handleOnChange, 500), []);
-
-    return <form className="inline-flex p-8 flex-col items-start rounded-2xl bg-white gap-12">
+    return <div className="flex flex-col gap-5 w-fit items-center">
+        <div className="w-[338px] h-[56px] p-6 shrink rounded-[20px] border-[1px] border-solid border-[#18BA51] flex items-center gap-2 shadow-[4px_4px_40px_0px_rgba(0,0,0,0.05)]">
+            <CheckIcon color="#18BA51"/>
+            <p className="text-[#18BA51] text-center">Нууц үг амжилттай солигдлоо</p>
+        </div>
+        
+        <form className="inline-flex p-8 flex-col items-start rounded-2xl bg-white gap-12">
         <div className="flex justify-center w-full">
             <h2 className="text-[#0D1118] text-center text-[28px] font-bold">Шинэ нууц үг зохиох </h2>
         </div>
@@ -31,4 +37,5 @@ export const RecoveryNewPassword = () => {
             <CustomButton type="submit" variant={formData.password.length > 0 && formData.rePassword.length > 0 ? "primary" : "secondary"}>Үргэлжлүүлэх</CustomButton>
         </div>
     </form>
+    </div>
 }
