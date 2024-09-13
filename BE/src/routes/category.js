f508c59fae4e956ca7feec0df28d432877f32c03
STRunit <st21unit@gmail.com>
2024-09-13 17:32:04 +0800

    Add get all categories query

--- BE/src/routes/category.js
+++ BE/src/routes/category.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createCategory } from "../controller/category.js";
+import { createCategory, getCategories } from "../controller/category.js";
 
 
 const categoryRouter = Router();
 
-categoryRouter.post("/create", createCategory);
+categoryRouter.post("/create", createCategory).get("/", getCategories);
 
 export {categoryRouter}
