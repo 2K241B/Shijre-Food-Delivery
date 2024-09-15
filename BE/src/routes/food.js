502c76f1b2517cfde3a9638b32e4a252493de8df
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-15 19:38:55 +0800

    Add update food query (#91)

--- BE/src/routes/food.js
+++ BE/src/routes/food.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createFood, getFoodByID, getFoods } from "../controller/food.js";
+import { createFood, getFoodByID, getFoods, updateFood } from "../controller/food.js";
 
 
 const foodRouter = Router();
@@ -7,7 +7,7 @@ const foodRouter = Router();
 foodRouter.post("/create", createFood)
 .get("/", getFoods)
 .get("/:id", getFoodByID)
-// .put("/:id", updateFood)
+.put("/:id", updateFood)
 // .delete("/:id", deleteFood);
 
 export {foodRouter}
