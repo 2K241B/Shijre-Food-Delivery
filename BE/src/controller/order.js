c96a79b15ca5734bfb2b2a048b91089cebce65b6
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-16 02:04:28 +0800

    Add get one order query

--- BE/src/controller/order.js
+++ BE/src/controller/order.js
@@ -25,6 +25,18 @@ export const getOrders = async (req, res) => {
     }
 }
 
+export const getOrderByID = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const response = await orderModel.findById(id);
+        res.send(response);
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error.message);
+    }
+}
+
 export const updateOrder = async (req, res) => {
     const { id } = req.params;
     const updatedValues = req.body
