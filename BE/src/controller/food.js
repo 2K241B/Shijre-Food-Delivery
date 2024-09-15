f84d3a4bb5bab01345974086602b23f926b4b7b2
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-15 19:24:30 +0800

    Add get all food query (#89)

--- BE/src/controller/food.js
+++ BE/src/controller/food.js
@@ -12,3 +12,15 @@ export const createFood = async (req, res) => {
         res.status(500).send(error.message);
     }
 }
+
+export const getFoods = async (req, res) => {
+
+    try {
+        const response = await foodModel.find()
+        res.send(response);
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error.message);
+    }
+}
