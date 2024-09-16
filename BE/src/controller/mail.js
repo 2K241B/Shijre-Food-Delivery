7d5de49bac888a0d13b4df9047e9954949d6e51e
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-17 00:42:28 +0800

    Add create OTP query (#116)

--- BE/src/controller/mail.js
+++ BE/src/controller/mail.js
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
-
+import { userModel } from "../schema/user.js"
+import { otpModel } from "../schema/otp.js"
 
 const transporter = nodemailer.createTransport({
     service: "Gmail",
@@ -7,24 +8,33 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-      user: "st21aye@gmail.com",
-      pass: "rqpx kpje xnqo qnbe",
+        user: "st21aye@gmail.com",
+        pass: "rqpx kpje xnqo qnbe",
     },
-  });
+});
 
 export const sendMail = async (req, res) => {
+    const { email } = req.body;
     try {
+        const user = userModel.findOne({ email });
 
-        const Info = await transporter.sendMail({
-            from: '"Maddison Foo Koch 👻" <st21aye@gmail.com>', // sender address
-            to: "dddelgersaihan@gmail.com", // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello World</b>", // html body
-        });
+        if (!user) return res.status(404).json("User not found");
+
+        const generatedOTP = Math.floor(Math.random() * 9000 + 1000);
+
+        await otpModel.create({ email, oneTimePass: generatedOTP });
+
+        const mailOptions = {
+            from: '"Food Delivery Project" <st21aye@gmail.com>',
+            to: email,
+            subject: "Password Reset OTP",
+            text: `Your One Time Password is: ${generatedOTP}. This Password will expire in 5 minutes.`
+        }
+
+        const Info = await transporter.sendMail(mailOptions);
 
         if (Info.messageId) {
-            res.status(200).send({success: true})
+            res.status(200).send({ success: true })
         } else {
             res.status(404).send({ error: "Message didn't send" })
         }
