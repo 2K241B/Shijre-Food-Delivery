9e9a19e3d4908fad84a40b17c2416a601f5426b9
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-17 17:57:31 +0800

    Add card component ui only (#124)

---
+++ FE/src/components/card.jsx
@@ -0,0 +1,24 @@
+"use client"
+import Image from "next/image"
+import { Discount } from "./discount"
+
+export const Card = ({ imageSrc, mainPrice, discountPercent, foodName }) => {
+    return <div className="inline-flex flex-col items-start gap-[14px]">
+        <div className="relative justify-center items-center flex w-[282px] h-[186px]">
+            <Image src={imageSrc} fill="true" className="rounded-2xl -z-10" />
+            <Discount percent={discountPercent} />
+        </div>
+
+        <div className="flex w-[282px] flex-col items-start gap-[2px]">
+            <p className="self-stretch text-xl font-semibold">{foodName}</p>
+            <div className="flex items-start gap-4">
+                {discountPercent ? (
+                    <p className="text-[#18BA51] text-lg font-semibold">{mainPrice - (mainPrice * discountPercent) / 100}₮</p>
+                ) : (
+                    <p className="text-[#18BA51] text-lg font-semibold">{mainPrice}₮</p>
+                )}
+                {discountPercent && <p className="text-[#171717] text-lg line-through">{mainPrice}₮</p>}
+            </div>
+        </div>
+    </div>
+}
