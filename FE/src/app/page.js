4e69084c6984ee3bb0b8e6171a7babdb0d92aebc
STRunit <st21unit@gmail.com>
2024-09-17 15:36:05 +0800

    Add create food component only ui

--- FE/src/app/page.js
+++ FE/src/app/page.js
@@ -1,3 +1,5 @@
+import { CreateFood } from "@/components/create-food";
+
 export default function Home() {
-  return <div></div>;
+  return <div><CreateFood/></div>;
 }
--- FE/src/components/customInput.jsx
+++ FE/src/components/customInput.jsx
@@ -4,8 +4,10 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { Switch } from "@/components/ui/switch"
 
-const CustomInput = React.forwardRef(({className, type, label, placeholder, showIcon = false, onchange, ...props }, ref) => {
+
+const CustomInput = React.forwardRef(({className, type, label, placeholder, showIcon = false, showSwitch = false, onchange, ...props }, ref) => {
 
     const [clicked, setClicked] = React.useState(true)
 
@@ -15,7 +17,10 @@ const CustomInput = React.forwardRef(({className, type, label, placeholder, show
 
     return (
         (<div className="flex flex-col items-start gap-1 self-stretch">
-            <p className="text-sm text-[#3F4145]">{label}</p>
+           <div className="flex gap-2 items-center">
+            {showSwitch ? <Switch/> : <></>}
+           <p className="text-sm text-[#3F4145]">{label}</p>
+           </div>
             <span className={cn(
                 "flex h-12 w-full rounded-sm border-[1px] border-solid border-[#ECEDF0] border-input bg-[#F7F7F8] px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 items-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
                 className
