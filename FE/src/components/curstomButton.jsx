9a56f2e606985ef21647851ca66b5b48708591ca
STRunit <st21unit@gmail.com>
2024-09-17 17:56:05 +0800

    Add card component ui only

--- FE/src/components/curstomButton.jsx
+++ FE/src/components/curstomButton.jsx
@@ -14,10 +14,12 @@ const buttonVariants = cva(
        dark: "bg-[#393939] text-[#FFFFFF] font-bold",
        ghost: "text-[#3F4145] font-bold",
        outline: "border-[1px] border-solid border-[#18BA51]",
+       light: "bg-white text-black font-semibold text-lg",
       },
       size: {
         default: "h-[48px] w-[384px] px-4 py-2",
         sm: "h-9 rounded-md px-4 py-[10px]",
+        md: "py-2 px-16 rounded-full",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
