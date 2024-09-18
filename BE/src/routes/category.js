9fdf1408e23b6ef7775c91cdaa0e9d56ff134723
STRunit <st21unit@gmail.com>
2024-09-18 16:27:59 +0800

    Add categories with foods query

--- BE/src/routes/category.js
+++ BE/src/routes/category.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createCategory, deleteCategory, getCategories, getCategoryByID, updateCategory } from "../controller/category.js";
+import { createCategory, deleteCategory, getCategories, getCategoriesWithFoods, getCategoryByID, updateCategory } from "../controller/category.js";
 
 
 const categoryRouter = Router();
 
 categoryRouter.post("/create", createCategory)
 .get("/", getCategories)
+.get("/foods",getCategoriesWithFoods)
 .get("/:id", getCategoryByID)
 .put("/:id", updateCategory)
 .delete("/:id", deleteCategory);
