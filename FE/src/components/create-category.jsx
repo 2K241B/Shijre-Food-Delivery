78f68779749da1eb77e293fa1ee8a4f08c4bc447
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-19 03:09:32 +0800

    Add create category component logic (#129)

--- FE/src/components/create-category.jsx
+++ FE/src/components/create-category.jsx
@@ -1,30 +1,71 @@
-import { CustomDialog, 
-    CustomDialogBody, 
-    CustomDialogContent, 
-    CustomDialogFooter, 
-    CustomDialogHeader, 
-    CustomDialogTitle, 
-    CustomDialogTrigger } from "@/components/customDialog";
-    import { CustomInput } from "@/components/customInput";
-    import { CustomButton } from "@/components/curstomButton";
+"use client"
+import {
+  CustomDialog,
+  CustomDialogBody,
+  CustomDialogContent,
+  CustomDialogFooter,
+  CustomDialogHeader,
+  CustomDialogTitle,
+  CustomDialogTrigger
+} from "@/components/customDialog";
+import { CustomInput } from "@/components/customInput";
+import { CustomButton } from "@/components/curstomButton";
 import { PlusIcon } from "lucide-react";
+import { useRef, useState } from "react";
+import { axiosInstance } from "@/lib/axios";
+import { Label } from "./ui/label";
 
 export const CreateCategory = () => {
-    return <CustomDialog>
+  const formRef = useRef(null);
+  const [output, setOutput] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(formRef.current);
+    const name = formData.get("categoryName");
+
+    try {
+      const response = await axiosInstance.post("/category/create", { name }, { withCredentials: true });
+
+      if (response.status === 200) {
+        setOutput("Category created successfully")
+        formRef.current.reset();
+      } else {
+        setOutput("Category creation failed");
+      }
+
+    } catch (error) {
+      setOutput(error)
+    }
+  }
+
+  const handleClear = () => {
+    if (formRef.current) {
+      formRef.current.reset();
+      setOutput("");
+    }
+
+  }
+
+  return <CustomDialog>
     <CustomDialogTrigger className="flex h-10 px-4 py-2 items-center gap-2 self-stretch rounded-[8px] border-[1px] border-solid border-[#D6D8DB] bg-[#fff] text-[#5E6166]">
-        <PlusIcon color="#5E6166"/>
-        Create New Category</CustomDialogTrigger>
+      <PlusIcon color="#5E6166" />
+      Create New Category</CustomDialogTrigger>
     <CustomDialogContent>
       <CustomDialogHeader className="border-b-[1px] flex">
         <CustomDialogTitle>Create New Category</CustomDialogTitle>
       </CustomDialogHeader>
-      <CustomDialogBody>
-      <CustomInput placeholder="Placeholder" className="" label="Category Name"/>
-      </CustomDialogBody>
-      <CustomDialogFooter>
-        <CustomButton variant="ghost" size="sm">Clear</CustomButton>
-        <CustomButton variant="dark" size="sm">Continue</CustomButton>
-      </CustomDialogFooter>
+      <form ref={formRef} onSubmit={handleSubmit}>
+        <CustomDialogBody>
+          <CustomInput name="categoryName" placeholder="Placeholder" className="" label="Category Name" />
+        <Label>{output}</Label>
+        </CustomDialogBody>
+        <CustomDialogFooter>
+          <CustomButton onClick={handleClear} type="button" variant="ghost" size="sm">Clear</CustomButton>
+          <CustomButton type="submit" variant="dark" size="sm">Continue</CustomButton>
+        </CustomDialogFooter>
+      </form>
     </CustomDialogContent>
   </CustomDialog>
 }
