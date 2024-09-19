2c0c06954a92f1a3a69bbc285bc6903151d6b2f7
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-20 01:54:29 +0800

    Add sidebar component logic

--- FE/src/components/sidebar.jsx
+++ FE/src/components/sidebar.jsx
@@ -3,23 +3,32 @@ import { axiosInstance } from "@/lib/axios";
 import { useEffect, useState } from "react"
 import { Category } from "./category";
 import { CreateCategory } from "./create-category";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export const Sidebar = () => {
     const [lists, setLists] = useState([]);
-  
+
     useEffect(() => {
         axiosInstance.get("/category/foods").then((res) => setLists(res.data));
     }, [])
-    
-    return <div className="flex w-[402px] h-screen py-[26px] pl-[120px] pr-[24px] flex-col items-start gap-10 shrink-0 bg-[#fff]">
-        <div className="flex items-center gap-[171px] self-stretch">
+
+    const searchParams = useSearchParams();
+    const categoryParam = searchParams.get("category")
+
+    return <div className="flex w-fit h-screen p-6 flex-col items-start gap-10 shrink-0 bg-[#fff]">
+        <div className="flex items-center self-stretch">
             <h3 className="text-[22px] font-bold">Food Menu</h3>
         </div>
-        <div className="flex flex-col items-start gap-[26px] self-stretch">
-            {lists.map((item) => {
-                return <Category key={item._id} name={item.name}/>
+        <div className="flex flex-col items-start gap-[26px] w-full">
+            {lists.map((category) => {
+                const isActive = category._id === categoryParam
+
+                return <Link key={category._id} className="self-stretch" href={{ pathname: '/admin', query: { category: category._id } }}>
+                    <Category isActive={isActive} key={category._id} name={category.name} />
+                </Link>
             })}
-            <CreateCategory/>
+            <CreateCategory />
         </div>
     </div>
 }
