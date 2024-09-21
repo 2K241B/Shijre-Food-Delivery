8e775ed7f84e38f498a9dc9fb4995784180fabb0
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-21 22:01:19 +0800

    Add admin dashboard page

--- FE/src/components/sidebar.jsx
+++ FE/src/components/sidebar.jsx
@@ -17,7 +17,7 @@ export const Sidebar = () => {
     const searchParams = useSearchParams();
     const categoryParam = searchParams.get("category")
 
-    return <div className="flex w-fit h-screen p-6 flex-col items-start gap-10 shrink-0 bg-[#fff]">
+    return <div className="flex w-fit h-[1285px] p-6 flex-col items-start gap-10 shrink-0 bg-[#fff]">
         <HeaderLabel name="Food Menu"/>
         <div className="flex flex-col items-start gap-[26px] w-full">
             {lists.map((category) => {
