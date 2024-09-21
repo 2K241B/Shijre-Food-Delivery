c22866207df577ffa72ce96daa2323cb06b8e3e0
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-22 03:04:04 +0800

    Add footer component (#156)

---
+++ FE/src/constants/index.js
@@ -0,0 +1,46 @@
+import { FacebookIcon, InstagramIcon, TwitterXIcon } from "@/components/icons";
+
+export const FOOTER_NAV_MENU_ITEMS = [
+    {
+        name: "Нүүр",
+        href: "/"
+    },
+    {
+        name: "Холбоо барих",
+        href: "/contact"
+    },
+    {
+        name: "Хоолны цэс",
+        href: "/category"
+    },
+    {
+        name: "Үйлчилгээний нөхцөл",
+        href: "/terms-of-service"
+    },
+    {
+        name: "Хүргэлтийн бүс",
+        href: "/delivery-zone"
+    },
+    {
+        name: "Нууцлалын бодлого",
+        href: "/privacy-policy"
+    },
+];
+
+export const SOCIAL_PLATFORMS = [
+    {
+        name: "Facebook",
+        icon: <FacebookIcon/>,
+        href: "https://www.facebook.com/"
+    },
+    {
+        name: "Instagram",
+        icon: <InstagramIcon/>,
+        href: "https://www.instagram.com/"
+    },
+    {
+        name: "X",
+        icon: <TwitterXIcon/>,
+        href: "https://x.com/home"
+    },
+]
