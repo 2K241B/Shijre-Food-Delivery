06dacc16b93c7c3eedf2e828f7c286b4084fd10c
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-21 18:27:30 +0800

    Add extra get all foods query (#147)

--- BE/src/routes/food.js
+++ BE/src/routes/food.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
-import { createFood, deleteFood, getFoodByID, getFoods, updateFood } from "../controller/food.js";
+import {
+    createFood,
+    deleteFood,
+    getAllFoods,
+    getFoodByID,
+    getFoods,
+    updateFood
+} from "../controller/food.js";
 
 
 const foodRouter = Router();
 
 foodRouter.post("/create", createFood)
-.get("/", getFoods)
-.get("/:id", getFoodByID)
-.put("/:id", updateFood)
-.delete("/:id", deleteFood);
+    .get("/", getFoods)
+    .get("/all", getAllFoods)
+    .get("/:id", getFoodByID)
+    .put("/:id", updateFood)
+    .delete("/:id", deleteFood);
 
-export {foodRouter}
+export { foodRouter }
