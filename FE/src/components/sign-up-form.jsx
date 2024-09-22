81cf74c22e4da4ce14da1d54cc872d35b7318aba
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-22 21:48:21 +0800

    Fix login and signup form position

--- FE/src/components/sign-up-form.jsx
+++ FE/src/components/sign-up-form.jsx
@@ -35,7 +35,7 @@ export const SignUpForm = () => {
         try {
             if (formData.password !== formData.rePassword) {
                 setWarning("Нууц үгээ дахин нягтална уу !")
-            } 
+            }
             else {
                 await axiosInstance.post(
                     "auth/sign-up",
@@ -54,30 +54,32 @@ export const SignUpForm = () => {
         }
     }
 
-    return <form onSubmit={handleSubmit} className="inline-flex p-8 flex-col items-start rounded-2xl bg-white gap-12">
-        <div className="flex justify-center w-full">
-            <h2 className="text-[#0D1118] text-center text-[28px] font-bold">Бүртгүүлэх</h2>
-        </div>
-        <div className="flex w-[384px] flex-col items-start gap-2">
-            <div className="flex flex-col items-start gap-4 self-stretch">
-                <CustomInput name="name" onchange={debounceFn} label={"Нэр"} placeholder={"Нэрээ оруулна уу"} />
-                <CustomInput name="email" type="email" onchange={debounceFn} label={"Имэйл"} placeholder={"Имэйл хаягаа оруулна уу"} />
-                <CustomInput name="phoneNumber" onchange={debounceFn} label={"Утас"} placeholder={"Та утасны дугаараа оруулна уу"} />
-                <CustomInput name="password" type="password" onchange={debounceFn} label={"Нууц үг"} placeholder={"Нууц үгээ оруулна уу"} showIcon={true} />
-                <CustomInput name="rePassword" type="password" onchange={debounceFn} label={"Нууц үг"} placeholder={"Нууц үгээ давтан оруулна уу"} showIcon={true} />
+    return <form onSubmit={handleSubmit} className="flex justify-center">
+        <div className="inline-flex p-8 flex-col items-start rounded-2xl bg-white gap-12">
+            <div className="flex justify-center w-full">
+                <h2 className="text-[#0D1118] text-center text-[28px] font-bold">Бүртгүүлэх</h2>
             </div>
-        </div>
-
-        <p className="text-sm text-red-400">{warning}</p>
-
-        <div className="flex flex-col items-center gap-8">
-            <div className="flex w-full items-center gap-2">
-                <Checkbox id="policy" onClick={handleChecked} />
-                <Label htmlFor="policy" className="text-sm text-[#3F4145]">
-                    Үйлчилгээний нөхцөл зөвшөөрөх
-                </Label>
+            <div className="flex w-[384px] flex-col items-start gap-2">
+                <div className="flex flex-col items-start gap-4 self-stretch">
+                    <CustomInput name="name" onchange={debounceFn} label={"Нэр"} placeholder={"Нэрээ оруулна уу"} />
+                    <CustomInput name="email" type="email" onchange={debounceFn} label={"Имэйл"} placeholder={"Имэйл хаягаа оруулна уу"} />
+                    <CustomInput name="phoneNumber" onchange={debounceFn} label={"Утас"} placeholder={"Та утасны дугаараа оруулна уу"} />
+                    <CustomInput name="password" type="password" onchange={debounceFn} label={"Нууц үг"} placeholder={"Нууц үгээ оруулна уу"} showIcon={true} />
+                    <CustomInput name="rePassword" type="password" onchange={debounceFn} label={"Нууц үг"} placeholder={"Нууц үгээ давтан оруулна уу"} showIcon={true} />
+                </div>
             </div>
-            <CustomButton type="submit" variant={formData.name.length > 0 && formData.email.length > 0 && formData.phoneNumber.length > 0 && formData.password.length > 0 && formData.rePassword.length > 0 && isChecked == true ? "primary" : "secondary"}>Бүртгүүлэх</CustomButton>
+
+            <p className="text-sm text-red-400">{warning}</p>
+
+            <div className="flex flex-col items-center gap-8">
+                <div className="flex w-full items-center gap-2">
+                    <Checkbox id="policy" onClick={handleChecked} />
+                    <Label htmlFor="policy" className="text-sm text-[#3F4145]">
+                        Үйлчилгээний нөхцөл зөвшөөрөх
+                    </Label>
+                </div>
+                <CustomButton type="submit" variant={formData.name.length > 0 && formData.email.length > 0 && formData.phoneNumber.length > 0 && formData.password.length > 0 && formData.rePassword.length > 0 && isChecked == true ? "primary" : "secondary"}>Бүртгүүлэх</CustomButton>
+            </div>
         </div>
     </form>
 }
