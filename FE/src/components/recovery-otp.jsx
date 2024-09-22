3d1dac2750b6f7fddaeef4f55b6c17a48368f8b3
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-23 06:41:11 +0800

    Add forgot password logic

--- FE/src/components/recovery-otp.jsx
+++ FE/src/components/recovery-otp.jsx
@@ -9,22 +9,52 @@ import {
     InputOTPSlot,
 } from "@/components/ui/input-otp"
 import { Label } from "./ui/label";
-
+import { useRouter, useSearchParams } from "next/navigation";
+import { axiosInstance } from "@/lib/axios";
 
 export const RecoveryOTP = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const email = searchParams.get("email");
+    const [value, setValue] = useState("");
 
-    const [value, setValue] = useState(0)
+    const handleClick = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axiosInstance.post("/recovery/verify-otp", {
+                email: email,
+                oneTimePass: value,
+            });
+
+            if (response.status === 200) {
+                alert('OTP verified successfully.');
+                const { email, accessTokens } = response.data;
+                router.push(`?step=3&email=${email}&token=${accessTokens}`);
+            } else {
+                alert('Failed to verify OTP. Please try again.');
+            }
+        } catch (error) {
+            console.error('Error verifying OTP:', error);
+            alert('An error occurred while verifying your OTP. Please try again later.');
+        }
+    };
+
 
     return <form className="inline-flex p-8 flex-col items-start rounded-2xl bg-white gap-12">
         <div className="flex justify-center w-full">
             <h2 className="text-[#0D1118] text-center text-[28px] font-bold">Нууц үг сэргээх</h2>
         </div>
         <div className="flex w-[384px] flex-col items-start gap-8">
-            <p className="text-center text-[#695C08] font-medium">Таны <a className="text-[#18BA51] font-medium leading-[22px]">example@pinecone.mn</a> хаяг руу сэргээх код илгээх болно. </p>
+            <p className="text-center text-[#695C08] font-medium">
+                Таны <b className="text-[#18BA51] font-medium leading-[22px]">{email}</b> хаяг руу сэргээх код илгээх болно. </p>
 
             <div className="flex flex-col items-center gap-4 self-stretch">
-                <Label htmlFor="inputOtp" className="font-normal text-sm text-[#3F4145]">Нууц үг сэргээх код</Label>
-                <InputOTP maxLength={4} id="inputOtp" name="inputOtp" onChange={() => setValue(value + 1)}>
+                <Label className="font-normal text-sm text-[#3F4145]">Нууц үг сэргээх код</Label>
+                <InputOTP
+                    maxLength={4}
+                    value={value}
+                    onChange={(value) => setValue(value)}>
                     <InputOTPGroup>
                         <InputOTPSlot index={0} />
                         <InputOTPSlot index={1} />
@@ -36,7 +66,10 @@ export const RecoveryOTP = () => {
             </div>
         </div>
         <div className="flex flex-col items-center gap-8">
-            <CustomButton type="submit" variant={value === 4 ? "primary" : "secondary"}>Үргэлжлүүлэх</CustomButton>
+            <CustomButton
+                onClick={handleClick}
+                type="submit"
+                variant={value.length === 4 ? "primary" : "secondary"}>Үргэлжлүүлэх</CustomButton>
         </div>
     </form>
 }
