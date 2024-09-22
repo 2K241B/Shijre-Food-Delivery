d817e4f0aee759e9498f0786bae5fa1ace7cb109
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-22 21:49:24 +0800

    Fix login and signup form position (#160)

--- FE/src/components/login-form.jsx
+++ FE/src/components/login-form.jsx
@@ -9,17 +9,17 @@ import _ from "lodash"
 
 export const LoginForm = () => {
     const router = useRouter();
-    const [formData, setFormData] = useState({ email: "", password: ""});
+    const [formData, setFormData] = useState({ email: "", password: "" });
 
     const handleOnChange = (event) => {
         setFormData((prev) => ({
             ...prev,
             [event.target.name]: event.target.value,
-          }));
+        }));
     }
 
     const debounceFn = useMemo(() => _.debounce(handleOnChange, 500), []);
-  
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -30,28 +30,30 @@ export const LoginForm = () => {
             })
 
             if (response.status === 200) router.push("/")
-            
+
         } catch (error) {
             console.error(error);
         }
     };
 
-    return <form onSubmit={handleSubmit} className="inline-flex p-8 flex-col items-start rounded-2xl bg-white gap-12">
-        <h2 className="text-[#0D1118] text-center text-[28px] font-bold">Нэвтрэх</h2>
-        <div className="flex w-[384px] flex-col items-start gap-2">
-            <div className="flex flex-col items-start gap-4 self-stretch">
-                <CustomInput name="email" type="email" onchange={debounceFn} label={"Имэйл"} placeholder={"Имэйл хаягаа оруулна уу"} />
-                <CustomInput name="password" type="password" onchange={debounceFn} label={"Нууц үг"} placeholder={"Нууц үг"} showIcon={true} />
+    return <form onSubmit={handleSubmit} className="flex justify-center">
+        <div className="inline-flex p-8 flex-col items-start rounded-2xl bg-white gap-12">
+            <h2 className="text-[#0D1118] text-center text-[28px] font-bold">Нэвтрэх</h2>
+            <div className="flex w-[384px] flex-col items-start gap-2">
+                <div className="flex flex-col items-start gap-4 self-stretch">
+                    <CustomInput name="email" type="email" onchange={debounceFn} label={"Имэйл"} placeholder={"Имэйл хаягаа оруулна уу"} />
+                    <CustomInput name="password" type="password" onchange={debounceFn} label={"Нууц үг"} placeholder={"Нууц үг"} showIcon={true} />
+                </div>
+                <p className="flex justify-end items-center text-sm self-stretch text-[#3F4145]">
+                    Нууц үг сэргээх
+                </p>
             </div>
-            <p className="flex justify-end items-center text-sm self-stretch text-[#3F4145]">
-                Нууц үг сэргээх
-            </p>
-        </div>
 
-        <div className="flex flex-col items-center gap-8">
-            <CustomButton type="submit" variant={formData.email && formData.password !== "" ? "primary" : "secondary"}>Нэвтрэх</CustomButton>
-            <p className="text-sm text-[#3F4145]">Эсвэл</p>
-            <CustomButton type="button" variant="outline">Бүртгүүлэх</CustomButton>
+            <div className="flex flex-col items-center gap-8">
+                <CustomButton type="submit" variant={formData.email && formData.password !== "" ? "primary" : "secondary"}>Нэвтрэх</CustomButton>
+                <p className="text-sm text-[#3F4145]">Эсвэл</p>
+                <CustomButton type="button" variant="outline">Бүртгүүлэх</CustomButton>
+            </div>
         </div>
     </form>
 }
