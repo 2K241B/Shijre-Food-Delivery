b55dee402dcaef1ef3fbf09cd2175c25c6d774f2
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-22 22:53:46 +0800

    Modify routes for password recovery

--- BE/src/routes/mail.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { sendMail, verifyOTP } from "../controller/mail.js";
-
-const mailRouter = Router();
-
-mailRouter.post("/", sendMail)
-    .post("/verifyOTP", verifyOTP);
-
-export { mailRouter }
