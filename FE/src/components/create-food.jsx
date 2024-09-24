2928e31c5c44c233622e305c3e8c5d0ef9d5f50e
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-24 15:47:22 +0800

    Refactor custom input (#173)

--- FE/src/components/create-food.jsx
+++ FE/src/components/create-food.jsx
@@ -21,11 +21,11 @@ export const CreateFood = () => {
                 <CustomDialogDescription></CustomDialogDescription>
             </CustomDialogHeader>
             <CustomDialogBody className="flex flex-col gap-4">
-                <CustomInput id="foodName" placeholder="Placeholder" className="" label="Хоолны нэр" />
-                <CustomInput id="foodCategory" placeholder="Placeholder" className="" label="Хоолны ангилал" />
-                <CustomInput id="foodIngredients" placeholder="Placeholder" className="" label="Хоолны орц" />
-                <CustomInput id="foodPrice" placeholder="Placeholder" className="" label="Хоолны үнэ" />
-                <CustomInput id="foodDiscount" placeholder="Placeholder" className="" label="Хямдралтай эсэх" showSwitch={true} />
+                <CustomInput name="foodName" id="foodName" placeholder="Placeholder" className="" label="Хоолны нэр" />
+                <CustomInput name="foodCategory" id="foodCategory" placeholder="Placeholder" className="" label="Хоолны ангилал" />
+                <CustomInput name="foodIngredients" id="foodIngredients" placeholder="Placeholder" className="" label="Хоолны орц" />
+                <CustomInput name="foodPrice" id="foodPrice" placeholder="Placeholder" className="" label="Хоолны үнэ" />
+                <CustomInput name="foodDiscount" id="foodDiscount" placeholder="Placeholder" className="" label="Хямдралтай эсэх" showSwitch={true} />
                 <div>
                     <CustomLabel className="font-normal text-[#121316] text-start mb-1">Хоолны зураг</CustomLabel>
                     <div className="p-2 flex flex-col gap-2 rounded-md border-dashed border-[#D6D7DC] border-[2px] items-center py-[25px] w-[284px] bg-[#BABCC41F]">
