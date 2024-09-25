ffaae5c74174fc37c515df8a442fb592e66728e8
STRunit <st21unit@gmail.com>
2024-09-25 15:49:48 +0800

    Add minor changes to sidebar components

--- FE/src/components/create-category.jsx
+++ FE/src/components/create-category.jsx
@@ -14,11 +14,9 @@ import { CustomButton } from "@/components/curstomButton";
 import { PlusIcon } from "lucide-react";
 import { useRef, useState } from "react";
 import { axiosInstance } from "@/lib/axios";
-import { CustomLabel } from "./customLabel";
 
 export const CreateCategory = () => {
   const formRef = useRef(null);
-  const [output, setOutput] = useState("")
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -30,23 +28,23 @@ export const CreateCategory = () => {
       const response = await axiosInstance.post("/category/create", { name }, { withCredentials: true });
 
       if (response.status === 200) {
-        setOutput("Category created successfully")
+        alert("Category created successfully")
         formRef.current.reset();
       } else {
-        setOutput("Category creation failed");
+        alert("Category creation failed");
       }
 
     } catch (error) {
-      setOutput(error)
+      console.error(error)
     }
+
+    location.reload()
   }
 
   const handleClear = () => {
     if (formRef.current) {
       formRef.current.reset();
-      setOutput("");
     }
-
   }
 
   return <CustomDialog>
@@ -61,7 +59,6 @@ export const CreateCategory = () => {
       <form ref={formRef} onSubmit={handleSubmit}>
         <CustomDialogBody>
           <CustomInput id="categoryId" name="categoryName" placeholder="Enter here ..." className="" label="Category Name" />
-          <CustomLabel>{output}</CustomLabel>
         </CustomDialogBody>
         <CustomDialogFooter>
           <CustomButton onClick={handleClear} type="button" variant="ghost" size="sm">Clear</CustomButton>
