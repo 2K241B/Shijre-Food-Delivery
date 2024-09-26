c70a3aea074fe5a6061b432bb56b3c285aa07bff
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-27 00:19:25 +0800

    Organize context provider (#189)

--- FE/src/app/admin/page.jsx
+++ FE/src/app/admin/page.jsx
@@ -9,9 +9,6 @@ import { ErrorIcon } from "@/components/icons/error-icon";
 import { Loading } from "@/components/loading";
 import { useCategoriesWithFoods } from "@/hooks/useCategoriesWithFoods";
 import { useSearchParams } from "next/navigation";
-import { createContext } from "react";
-
-export const CategoriesWithFoodsContext = createContext();
 
 const AdminPage = () => {
     const search = useSearchParams();
@@ -22,38 +19,36 @@ const AdminPage = () => {
 
     if (loading) return <Loading />
     if (error) return
-    <Error errorMessage="Уучлаарай систем ачааллахад алдаа гарлаа.">
+    (<Error errorMessage="Уучлаарай систем ачааллахад алдаа гарлаа.">
         <ErrorIcon />
-    </Error>
+    </Error>)
 
     return (
-        <CategoriesWithFoodsContext.Provider value={{ response }}>
-            <div className="p-6 w-full flex flex-col gap-8">
-                <div className="flex justify-between w-full p-4">
-                    <HeaderLabel 
+        <div className="p-6 w-full flex flex-col gap-8">
+            <div className="flex justify-between w-full p-4">
+                <HeaderLabel
                     name={name} />
-                    <CreateFood />
-                </div>
-
-                <div className="grid grid-cols-3 gap-6 justify-center">
-                    {foods && foods.length > 0 ? (
-                        foods.map((food) => (
-                            <Card key={food._id}
-                                mainPrice={food.price}
-                                discountPercent={food.discount}
-                                foodName={food.name}
-                                imageSrc={food.image}
-                                alt={food.name} />
-                        ))
-                    ) : (
-                        <div className="col-start-2">
-                            <AddIcon className="mx-auto" />
-                            <p className="text-[#808080] text-center mt-10">Уучлаарай, Таны меню хоосон байна.</p>
-                        </div>
-                    )}
-                </div>
+                <CreateFood />
             </div>
-        </CategoriesWithFoodsContext.Provider>
+
+            <div className="grid grid-cols-3 gap-6 justify-center">
+                {foods && foods.length > 0 ? (
+                    foods.map((food) => (
+                        <Card key={food._id}
+                            mainPrice={food.price}
+                            discountPercent={food.discount}
+                            foodName={food.name}
+                            imageSrc={food.image}
+                            alt={food.name} />
+                    ))
+                ) : (
+                    <div className="col-start-2">
+                        <AddIcon className="mx-auto" />
+                        <p className="text-[#808080] text-center mt-10">Уучлаарай, Таны меню хоосон байна.</p>
+                    </div>
+                )}
+            </div>
+        </div>
     );
 }
 
