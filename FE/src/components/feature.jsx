3db551496d58b04ec54c80d7ec80997427c133b6
STRunit <146666238+STRunit@users.noreply.github.com>
2024-09-26 15:29:00 +0800

    Add category feature component (#184)

--- FE/src/components/feature.jsx
+++ FE/src/components/feature.jsx
@@ -1,6 +1,3 @@
-import { BookIcon } from "./icons/book-icon"
-
-
 export const Feature = ({ children, label, content }) => {
     return <div className="flex p-4 flex-col justify-center items-start gap-[15px] flex-1 self-stretch rounded-2xl border-solid border-[#D6D8DB] shadow-[4px_4px_12px_0_rgba(0,0,0,0.10)]">
         <div className="flex p-[15px] flex-col justify-center items-center gap-[10px]">
